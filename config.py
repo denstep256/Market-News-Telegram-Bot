0ed3a4712d5caaffee7f6e4ea3306a7476ae2fea
denstep256 <stepanenko-200@bk.ru>
2025-04-13 20:26:34 +0300

    add README

---
+++ config.py
@@ -0,0 +1,7 @@
+TOKEN='7727318535:AAHs5022c04Dx_M7rYXvYa9NZ6dFJuj8TaM'
+
+DB_URL_USERS='sqlite+aiosqlite:///db.sqlite3'
+DB_URL_KEYS='sqlite:///issued_keys.sqlite3'
+
+CMC_API='9a041466-a881-4fa8-9ed7-9683d409fecf'
+
